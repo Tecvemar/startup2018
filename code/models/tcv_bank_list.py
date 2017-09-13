8752a53671fce8683f7e38195beb3bdf672beae0
DDBCDVD <dbcdvd@gmail.com>
2017-09-13 12:02:08 -0400

    tcv_bank_list processed

--- code/models/tcv_bank_list.py
+++ code/models/tcv_bank_list.py
@@ -7,6 +7,5 @@ def load_tcv_bank_list(lnk):
     c2o = csv_2_openerp(
         '../data/common/tcv_bank_list.csv', 'tcv.bank.list', lnk)
     c2o.set_search_fields(['name'])
-    #~ c2o.set_integer_fields(['code'])
     c2o.set_boolean_fields(['active'])
     c2o.process_csv()
