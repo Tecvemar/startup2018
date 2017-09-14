318500f03c5c30461ba2e64456217cdb6169ae47
DDBCDVD <dbcdvd@gmail.com>
2017-09-14 14:40:46 -0400

    product_category.py  processed

--- code/models/product_category.py
+++ code/models/product_category.py
@@ -7,7 +7,6 @@ def load_product_category(lnk):
     c2o = csv_2_openerp(
         '../data/common/product_category.csv', 'product.category', lnk)
     c2o.set_search_fields(['code'])
-    #~ c2o.set_integer_fields(['property_stock_variation'])
     c2o.set_relational_fields([
         ('property_stock_account_output_categ', 'account.account', ['code']),
         ('property_stock_account_input_categ', 'account.account', ['code']),
