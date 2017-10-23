96daa3af131bbbb63c8a730c67832d5ca3885387
juanvmarquezl <juanvmarquezl@gmail.com>
2017-10-23 09:47:43 -0400

    tcv_import_config

--- code/models/__init__.py
+++ code/models/__init__.py
@@ -27,3 +27,4 @@ from tcv_igtf import load_tcv_igtf
 from account_fiscalyear import load_account_fiscalyear
 from account_period import load_account_period
 from res_partner_account import load_res_partner_account
+from tcv_import_config import load_tcv_import_config
---
+++ code/models/tcv_import_config.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+from csv2open import csv_2_openerp
+
+
+def load_tcv_import_config(lnk):
+
+    c2o = csv_2_openerp(
+        '../data/common/tcv_import_config.csv', 'res.company', lnk)
+    c2o.set_search_fields(['id'])
+    c2o.set_integer_fields(['id'])
+    c2o.set_relational_fields([
+        ('journal_id', 'account.journal', ['name']),
+        ('account_id', 'account.account', ['code']),
+        ])
+    c2o.update_records = True
+    c2o.process_csv()
