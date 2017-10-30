27db25d5cdb6a665fa389a9cf9b018d388e8dfdd
DDBCDVD <dbcdvd@gmail.com>
2017-10-30 15:43:11 -0400

    stock_move in process

---
+++ code/companies/stock_move.py
@@ -0,0 +1,53 @@
+# -*- encoding: utf-8 -*-
+from profit2open import profit_2_openerp
+from csv2open import csv_2_openerp
+import os
+
+
+def load_lots(dbcomp, dbprofit):
+    if not dbprofit:
+        return
+    p2o = profit_2_openerp('stock.move', dbcomp, dbprofit)
+    p2o.set_sql(
+        '''
+select rtrim(r.nro_lote) as name, rtrim(r.co_art) as product_id,
+       e.fec_emis as date, rtrim(n.aux02) as aux02,
+       n.prec_vta as property_cost_price, sum(r.total_art) as product_qty, 'm2' as product_uom
+from factura f
+left join reng_fac r on r.fact_num = f.fact_num
+left join reng_com n on r.nro_lote = n.nro_lote and r.co_art = n.co_art
+left join compras e on n.fact_num = e.fact_num
+where  f.fec_emis  >= '2017-01-01' and
+      e.fec_emis < '2017-01-01' and
+      rtrim(r.nro_lote) != ''
+group by r.nro_lote, r.co_art, e.fec_emis, n.aux02, n.prec_vta
+        ''')
+
+    p2o.set_search_fields(['name', 'product_id'])
+    p2o.set_relational_fields([
+        ('product_id', 'product.product', ['default_code']),
+        ('product_uom', 'product.uom', ['name']),
+        ('location_id', 'stock.location', ['name']),
+        ])
+    p2o.set_aux02_fields(['heigth', 'length'])
+    p2o.process_csv()
+    #~ p2o.test_data_file()
+
+
+#~ def load_lots(dbcomp, dbprofit):
+    #~ '''
+    #~ Load extra lots from Profit data -> csv
+    #~ '''
+
+    #~ work_dir = '../data/companies/%s/' % dbcomp.database
+    #~ work_csv = work_dir + 'stock_move.csv'
+    #~ if not os.path.isfile(work_csv):
+        #~ return
+    #~ c2o = csv_2_openerp(work_csv, 'stock.move', dbcomp)
+    #~ c2o.set_search_fields(['name', 'product_id'])
+    #~ c2o.set_relational_fields([
+        #~ ('product_id', 'product.product', ['default_code']),
+        #~ ])
+    #~ c2o.set_aux02_fields(['heigth', 'length'])
+    #~ c2o.process_csv()
+    #~ c2o.test_data_file()
--- code/load_companies_data.py
+++ code/load_companies_data.py
@@ -6,7 +6,7 @@ import companies
 
 
 #~ for database in ['monagas']:
-for database in dbdata['databases'][:6]:
+for database in dbdata['databases'][:1]:
     if dbdata[database]['profit']:
         lnk_dbprofit = profit_link(
             dbdata[database]['profit']['host'],
@@ -24,23 +24,24 @@ for database in dbdata['databases'][:6]:
         dbdata['openerp_password'])
 
     print 'Cargando datos de compañias: %s...' % database
-
-    companies.load_bank_account_journal(lnk_dbcom, lnk_dbprofit)
-    companies.load_stock_production_lot(lnk_dbcom, lnk_dbprofit)
-    companies.load_stock_production_lot_extra(lnk_dbcom)
-    companies.load_res_company(lnk_dbcom)
-    companies.load_stock_warehouse(lnk_dbcom)
-    companies.load_sale_shop(lnk_dbcom)
-    companies.load_account_account_0(lnk_dbcom)
-    companies.load_product_product(lnk_dbcom)
-    companies.load_res_partner_address(lnk_dbcom)
-    companies.load_res_partner(lnk_dbcom)
-    companies.load_res_partner_profit_pruchase(lnk_dbcom, lnk_dbprofit)
-    companies.load_purchase_order(lnk_dbcom, lnk_dbprofit)
-    companies.load_purchase_order_line_profit_detail(lnk_dbcom, lnk_dbprofit)
-    companies.load_purchase_order_no_details(lnk_dbcom, lnk_dbprofit)
-    companies.postprocess_purchase_order(lnk_dbcom, lnk_dbprofit)
-    companies.load_res_partner_profit_sale(lnk_dbcom, lnk_dbprofit)
-    companies.load_res_partner_companies_extra_data(lnk_dbcom)
-    companies.load_sale_order(lnk_dbcom, lnk_dbprofit)
-    companies.load_sale_order_line_profit_detail(lnk_dbcom, lnk_dbprofit)
+#~
+    #~ companies.load_bank_account_journal(lnk_dbcom, lnk_dbprofit)
+    #~ companies.load_stock_production_lot(lnk_dbcom, lnk_dbprofit)
+    #~ companies.load_stock_production_lot_extra(lnk_dbcom)
+    #~ companies.load_res_company(lnk_dbcom)
+    #~ companies.load_stock_warehouse(lnk_dbcom)
+    #~ companies.load_sale_shop(lnk_dbcom)
+    #~ companies.load_account_account_0(lnk_dbcom)
+    #~ companies.load_product_product(lnk_dbcom)
+    #~ companies.load_res_partner_address(lnk_dbcom)
+    #~ companies.load_res_partner(lnk_dbcom)
+    #~ companies.load_res_partner_profit_pruchase(lnk_dbcom, lnk_dbprofit)
+    #~ companies.load_purchase_order(lnk_dbcom, lnk_dbprofit)
+    #~ companies.load_purchase_order_line_profit_detail(lnk_dbcom, lnk_dbprofit)
+    #~ companies.load_purchase_order_no_details(lnk_dbcom, lnk_dbprofit)
+    #~ companies.postprocess_purchase_order(lnk_dbcom, lnk_dbprofit)
+    #~ companies.load_res_partner_profit_sale(lnk_dbcom, lnk_dbprofit)
+    #~ companies.load_res_partner_companies_extra_data(lnk_dbcom)
+    #~ companies.load_sale_order(lnk_dbcom, lnk_dbprofit)
+    #~ companies.load_sale_order_line_profit_detail(lnk_dbcom, lnk_dbprofit)
+    companies.load_lots(lnk_dbcom, lnk_dbprofit)
