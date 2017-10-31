dc0b7f384c3c89178337f0eb26224582a15a1054
DDBCDVD <dbcdvd@gmail.com>
2017-10-31 12:22:25 -0400

    stock_inventory process

--- code/models/stock_location.py
+++ code/models/stock_location.py
@@ -7,7 +7,9 @@ def load_stock_location(lnk):
     c2o = csv_2_openerp(
         '../data/common/stock_location.csv', 'stock.location', lnk)
     c2o.set_search_fields(['name'])
-    c2o.set_boolean_fields(['active'])
+    c2o.set_relational_fields([
+        ('location_id', 'stock.location', ['name']),
+        ])
     c2o.update_records = True
     c2o.process_csv()
     #~ c2o.test_data_file()
