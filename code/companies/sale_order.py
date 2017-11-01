c120b16d0249a239ba93abfdc5dfb7656a5797a7
juanvmarquezl <juanvmarquezl@gmail.com>
2017-11-01 16:45:51 -0400

    sale_order.date_due

--- code/companies/sale_order.py
+++ code/companies/sale_order.py
@@ -15,7 +15,8 @@ select c.fec_emis as date_order, nro_doc as partner_ref,
        rtrim(tipo_doc) + '-' + ltrim(str(nro_doc)) as client_order_ref,
        rtrim(co_cli) as partner_id, 'Contado' as payment_term,
        'Stock' as location_id, 'Public Pricelist' as pricelist_id,
-       'one' as picking_policy, 'prepaid' as order_policy
+       'one' as picking_policy, 'prepaid' as order_policy,
+       '2017-12-31' as date_due
 from docum_cc c
 where tipo_doc = 'FACT' and fec_emis >= '2017-01-01' and anulado = 0
 order by nro_doc
