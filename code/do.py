44c490d63c099f8f3df4d5b36e41270785f64fb5
juanvmarquezl <juanvmarquezl@gmail.com>
2017-11-02 16:46:58 -0400

    Split 'do.py' process

--- code/do.py
+++ code/do.py
@@ -8,4 +8,4 @@ x = raw_input('Favor DETENER el servidor Open y pgadmin para continuar y pulse [
 import backup_bdd_ref
 import create_bdd_companies
 x = raw_input('Favor INICIAR el servidor OpenERP para continuar y pulse [Enter]...')
-import load_companies_data
+print "Ejecute: load_companies_data"
