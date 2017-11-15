49474dcebddbb9044fc3870c51fe6bfa339a9e86
Juan Márquez <juanvmarquezl@users.noreply.github.com>
2017-11-15 09:01:04 -0400

    Update load_companies_data.py

--- code/load_companies_data.py
+++ code/load_companies_data.py
@@ -24,6 +24,7 @@ for database in dbdata['databases'][:6]:
         dbdata['migracion_password'])
 
     print 'Cargando datos de compañias: %s...' % database
+    
     companies.load_bank_account_journal(lnk_dbcom, lnk_dbprofit)
     companies.load_stock_production_lot(lnk_dbcom, lnk_dbprofit)
     companies.load_stock_production_lot_extra(lnk_dbcom)
